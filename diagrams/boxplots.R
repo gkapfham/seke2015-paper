940fefda30e5100beb10d624d3fb43d9ac32b59e
Cody Kinneer <kinneerc@allegheny.edu>
2015-02-26 18:07:48 -0500

    fixed boxplot labels

--- diagrams/boxplots.R
+++ diagrams/boxplots.R
@@ -1,4 +1,8 @@
+library(plyr)
+library(ggplot2)
+
+independentFull <- independent
 independentFull$Criterion <- revalue(independent$Criterion,c("AI"="AICC","AN"="ANCC","AP"="APC","AU"="AUCC","I"="ICC","lAI"="ClauseAICC","N"="NCC","oAI"="CondAICC","U"="UCC"))
-ggplot(data=independentFull,aes(x=Criterion,y=log(min))) + geom_boxplot() + theme(axis.text.x = element_text(angle = 35, hjust = 1)) + ggtitle("Criterion vs Time") + ylab("Time log(minuites)")
-independentFull$DataGenerator <- revalue(independent$DataGenerator,c("a"="avs","aD"="avs defaults","dR"="directed random","dRD"="directed random defaults","r"="random","rD"="random defaults"))
-ggplot(data=independentFull,aes(x=DataGenerator,y=log(min))) + geom_boxplot() + theme(axis.text.x = element_text(angle = 35, hjust = 1)) + ggtitle("Data Generator vs Time") + ylab("Time log(minuites)") + xlab("Data Generator")
+ggplot(data=independentFull,aes(x=Criterion,y=log(min))) + geom_boxplot() + theme(axis.text.x = element_text(angle = 35, hjust = 1)) + ggtitle("Criterion vs log(Minutes)") + ylab("Time log(minutes)")
+independentFull$DataGenerator <- revalue(independent$DataGenerator,c("a"="AVM","aD"="AVM Defaults","dR"="Directed Random","dRD"="Directed Random Defaults","r"="Random","rD"="Random Defaults"))
+ggplot(data=independentFull,aes(x=DataGenerator,y=log(min))) + geom_boxplot() + theme(axis.text.x = element_text(angle = 35, hjust = 1)) + ggtitle("Data Generator vs log(Minutes)") + ylab("Time log(minutes)") + xlab("Data Generator")
